d7d68bb18821daf6aec24df25737633b4adba57d
schen08 <sunnychen775@gmail.com>
2025-09-01 16:52:55 -0700

    created ai provider

---
+++ lib/providers/ai_provider.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import '../services/ai_service.dart';
+
+class AIProvider extends ChangeNotifier {
+  late final AIService _ai;
+
+  AIProvider() {
+    _ai = AIService();
+  }
+
+  //provider creates a middle ground for relaying information between UI code and the AI Service
+  Future<Map<String, dynamic>> generateContent(String unitTitle) async {
+    return await _ai.generateContent(unitTitle);
+  }
+
+  Future<Map<String, dynamic>> generateFeedback(
+    String scenario,
+    String userResponse,
+  ) async {
+    return await _ai.generateFeedback(scenario, userResponse);
+  }
+}
