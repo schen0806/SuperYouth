1a661d4d9461fa235a3e8932ec9393a905b2b805
schen08 <sunnychen775@gmail.com>
2025-09-14 11:43:29 -0700

    Created feedback screen to save user progress

--- lib/screens/feedback_screen.dart
+++ lib/screens/feedback_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'package:super_youth/data/unit.dart';
+import 'package:super_youth/providers/auth_provider.dart';
 import 'package:super_youth/widgets/nav_drawer.dart';
 
 import '../providers/ai_provider.dart';
@@ -25,6 +26,29 @@ class FeedbackScreen extends StatefulWidget {
 }
 
 class _FeedbackScreenState extends State<FeedbackScreen> {
+  late Future<Map<String, dynamic>> feedbackData;
+
+  @override
+  void initState() {
+    super.initState();
+    final authProvider = Provider.of<AuthenticationProvider>(
+      context,
+      listen: false,
+    );
+    feedbackData = Provider.of<AIProvider>(
+      context,
+      listen: false,
+    ).generateFeedback(widget.scenario, widget.userResponse).then((feedback) {
+      authProvider.updateProgress(
+        widget.unitNumber,
+        feedback,
+        widget.scenario,
+        widget.userResponse,
+      );
+      return feedback;
+    });
+  }
+
   String _buildScoreMessage(int score) {
     if (score >= 9)
       return "Amazing";
@@ -58,15 +82,12 @@ class _FeedbackScreenState extends State<FeedbackScreen> {
         //futurebuilder builds the UI from the future
         FutureBuilder(
           //get a provider form a widget class
-          future: Provider.of<AIProvider>(
-            context,
-            listen: false,
-          ).generateFeedback(widget.scenario, widget.userResponse),
+          future: feedbackData,
           builder: (
             BuildContext context,
             AsyncSnapshot<Map<String, dynamic>> snapshot,
           ) {
-            if (snapshot.hasData) {
+            if (snapshot.connectionState == ConnectionState.done) {
               //access the feedback/actual data in the s
               //maps are dictionaries in flutter-key-value pair
               Map<String, dynamic> feedback = snapshot.data!;
@@ -95,6 +116,10 @@ class _FeedbackScreenState extends State<FeedbackScreen> {
                     Text(
                       _buildBulletPoints("Cons", feedback["cons"]),
                       style: TextTheme.of(context).bodyLarge,
+                    ),
+                    Text(
+                      "Model response: \n${feedback["Model Response"]}",
+                      style: TextTheme.of(context).bodyMedium,
                     ),
                     ElevatedButton.icon(
                       onPressed: () {
